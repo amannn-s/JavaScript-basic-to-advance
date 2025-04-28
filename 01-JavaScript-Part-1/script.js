088060567adbb71c94006bf534f031a6b5ac8fc8
amannn-s <s.aman230630@gmail.com>
2025-04-28 10:23:23 +0530

    Complete Part 1 and Started Part 2

--- 01-JavaScript-Part-1/script.js
+++ 01-JavaScript-Part-1/script.js
@@ -426,11 +426,54 @@ if (day == "monday") {
   console.log("Not a valid day!!!");
 }
 
+/////////////////////////////////////////////////////
+//* Statements(line) and Expressions(words)
+3 + 4;
+1991;
+true && false && !false;
+
+if (23 > 10) {
+  const str = "23 is greater";
+}
+
+const me = "Rahul";
+console.log(`I'm ${2025 - 1998} years old ${me}`);
+
+/////////////////////////////////////////////////////
+// Ternary operator (Expression)
+const age = 22;
+console.log(`You ${age >= 18 ? "can" : "cannot"} drive a car`);
+
+let drive;
+
+drive = age >= 18 ? "Car" : "Cycle";
+
+// if (age >= 18) {
+//   drive = "Car";
+// } else {
+//   drive = "Cycle"
+// }
+
+console.log(drive);
+
 */
+
+/////////////////////////////////////////////////////
+//* Coding Challenge #4
+
+// Rahul wants to build a very simple tip calculator for whenever he goes eating in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
+
+// 1. Your task is to calculate the tip, depending on the bill value. Create a variable called 'tip' for this. It's not allowed to use an if/else statement (If it's easier for you, you can start with an if/ else statement, and then try to convert it to a ternary operator!)
+
+// 2. Print a string to the console containing the bill value, the tip, and the final value (bill + tip). Example: 'The bill was 275, the tip was 41.25, and the total is 316.25.'
+
+// TEST DATA:
+// value 316.25, 275, 40 and 430
+
 /////////////////////////////////////////////////////
 //* Complete part 1
 
-console.log("End of a file");
+// console.log("End of a file");
 
 // Feature Sum
 function sum(x, y) {
